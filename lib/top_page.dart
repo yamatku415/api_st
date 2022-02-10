b4c75dc2d95ac65133663a415403ba6613e7bd05
yamatku415 <87381371+yamatku415@users.noreply.github.com>
2022-02-10 17:11:16 +0900

    '背景追加'

--- lib/top_page.dart
+++ lib/top_page.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lab_api_st/weather.dart';
 import 'package:lab_api_st/zip_code.dart';
+
+import 'app_back_ground.dart';
 
 class TopPage extends StatefulWidget {
   const TopPage({Key? key}) : super(key: key);
@@ -24,172 +25,176 @@ class _TopPageState extends State<TopPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Column(
-      children: [
-        Container(
-          width: 200,
-          child: TextField(
-            onSubmitted: (value) async {
-              //TextFieldで打ち込まれた値をvalueに入れて何かの処理に使うことができる。
-              print(value);
-              Map<String, String>? response = {};
-              response = await ZipCode.searchAddressFromZipCode(value);
-              errorMessage = response?['message'];
-              if (response!.containsKey('address')) {
-                address = response['address'] ?? '';
-                currentWeather = await Weathaer.getCurrentWeather(value);
-                Map<String, List<Weathaer>>? weatherForecast =
-                    await Weathaer.getForecast(
-                        lon: currentWeather!.lon, lat: currentWeather!.lat);
-                hourlyWeather = weatherForecast?['hourly'];
-                dailyWeather = weatherForecast?['daily'];
-              }
+            child: Stack(children: [
+      AppBackground(),
+      Column(
+        children: [
+          Container(
+            width: 200,
+            child: TextField(
+              onSubmitted: (value) async {
+                //TextFieldで打ち込まれた値をvalueに入れて何かの処理に使うことができる。
+                print(value);
+                Map<String, String>? response = {};
+                response = await ZipCode.searchAddressFromZipCode(value);
+                errorMessage = response?['message'];
+                if (response!.containsKey('address')) {
+                  address = response['address'] ?? '';
+                  currentWeather = await Weathaer.getCurrentWeather(value);
+                  Map<String, List<Weathaer>>? weatherForecast =
+                      await Weathaer.getForecast(
+                          lon: currentWeather!.lon, lat: currentWeather!.lat);
+                  hourlyWeather = weatherForecast?['hourly'];
+                  dailyWeather = weatherForecast?['daily'];
+                }
 
-              ///searchAddressFromZipCodeでasync/awaitを使っているからこっちでもasync/awaitを使ってあげる
-              print(address);
-              print(hourlyWeather);
+                ///searchAddressFromZipCodeでasync/awaitを使っているからこっちでもasync/awaitを使ってあげる
+                print(address);
+                print(hourlyWeather);
 
-              setState(() {});
-            },
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(hintText: '郵便番号入力'),
-          ),
-        ),
-        Text(
-          errorMessage == null ? '' : errorMessage!,
-          style: TextStyle(color: Colors.red),
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        Text(
-          address,
-          style: const TextStyle(fontSize: 25),
-        ),
-        Text(currentWeather == null ? '-' : currentWeather!.description ?? ''),
-        Text(
-          currentWeather == null
-              ? '-'
-              : '${currentWeather!.temp!.toStringAsFixed(0)}°',
-          style: const TextStyle(fontSize: 80),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8.0),
-              child: Text(currentWeather == null
-                  ? '最高:-'
-                  : '最高:${currentWeather!.tempMax!.toStringAsFixed(0)}°'),
+                setState(() {});
+              },
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(hintText: '郵便番号入力'),
             ),
-            Text(currentWeather == null
-                ? '最低:-'
-                : '最低:${currentWeather!.tempMin!.toStringAsFixed(0)}°'),
-          ],
-        ),
-        const SizedBox(
-          height: 50,
-        ),
-        const Divider(
-          height: 0,
-        ),
-        SingleChildScrollView(
-          //wodgetをスクロール可能にする
-          scrollDirection: Axis
-              .horizontal, //SingleChildScrollViewは縦方向のスクロールがデフォルトだからこれで横方向に適応させる。
+          ),
+          Text(
+            errorMessage == null ? '' : errorMessage!,
+            style: TextStyle(color: Colors.red),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          Text(
+            address,
+            style: const TextStyle(fontSize: 25),
+          ),
+          Text(
+              currentWeather == null ? '-' : currentWeather!.description ?? ''),
+          Text(
+            currentWeather == null
+                ? '-'
+                : '${currentWeather!.temp!.toStringAsFixed(0)}°',
+            style: const TextStyle(fontSize: 80),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Text(currentWeather == null
+                    ? '最高:-'
+                    : '最高:${currentWeather!.tempMax!.toStringAsFixed(0)}°'),
+              ),
+              Text(currentWeather == null
+                  ? '最低:-'
+                  : '最低:${currentWeather!.tempMin!.toStringAsFixed(0)}°'),
+            ],
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Divider(
+            height: 0,
+          ),
+          SingleChildScrollView(
+            //wodgetをスクロール可能にする
+            scrollDirection: Axis
+                .horizontal, //SingleChildScrollViewは縦方向のスクロールがデフォルトだからこれで横方向に適応させる。
 
-          child: hourlyWeather == null
-              ? Container()
-              : Row(
-                  children: hourlyWeather!.map((weatherPre) {
-                    ///mapの書き方　チャプター5　8:00　weatherPreの所はなんでもいい
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8.0, vertical: 8.0),
-                      child: Column(
-                        children: [
-                          Text(
-                              '${DateFormat('H').format(weatherPre.time!)}時'), //hourlyWeather[0].0を書く事でリストの一番最初のモノを取得出来る
+            child: hourlyWeather == null
+                ? Container()
+                : Row(
+                    children: hourlyWeather!.map((weatherPre) {
+                      ///mapの書き方　チャプター5　8:00　weatherPreの所はなんでもいい
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0, vertical: 8.0),
+                        child: Column(
+                          children: [
+                            Text(
+                                '${DateFormat('H').format(weatherPre.time!)}時'), //hourlyWeather[0].0を書く事でリストの一番最初のモノを取得出来る
 
-                          Image.network(
-                              'http://openweathermap.org/img/wn/${weatherPre.icon}.png'),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0),
-                            child: Text(
-                              '${weatherPre.temp!.toStringAsFixed(0)}°',
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                ),
-        ),
-        const Divider(
-          height: 0,
-        ),
-        dailyWeather == null
-            ? Container()
-            : Expanded(
-                //適応範囲を限定できる（SingleChildScrollViewを使うときにエラーが出たら使うぐらいで覚えとけばいい、とりあえず）
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Column(
-                      children: dailyWeather!.map((we) {
-                        return Container(
-                          height: 50,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                  width: 90,
-                                  child: Text(
-                                      '${weekDay[we.time!.weekday - 1]}曜日')),
-                              Row(
-                                children: [
-                                  Image.network(
-                                      'http://openweathermap.org/img/wn/${we.icon}.png'),
-                                  Container(
-                                    width: 50,
-                                    child: Text(
-                                      '${we.rainyPercent}%',
-                                      style: const TextStyle(
-                                          color: Colors.lightBlueAccent),
-                                    ),
-                                  ),
-                                ],
+                            Image.network(
+                                'http://openweathermap.org/img/wn/${weatherPre.icon}.png'),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0),
+                              child: Text(
+                                '${weatherPre.temp!.toStringAsFixed(0)}°',
+                                style: const TextStyle(fontSize: 18),
                               ),
-                              Container(
-                                width: 90,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
+                            )
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+          ),
+          const Divider(
+            height: 0,
+          ),
+          dailyWeather == null
+              ? Container()
+              : Expanded(
+                  //適応範囲を限定できる（SingleChildScrollViewを使うときにエラーが出たら使うぐらいで覚えとけばいい、とりあえず）
+                  child: SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Column(
+                        children: dailyWeather!.map((we) {
+                          return Container(
+                            height: 50,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                    width: 90,
+                                    child: Text(
+                                        '${weekDay[we.time!.weekday - 1]}曜日')),
+                                Row(
                                   children: [
-                                    Text(
-                                      '↑${we.tempMax!.toStringAsFixed(0)}°',
-                                      style: const TextStyle(
-                                          fontSize: 16, color: Colors.red),
-                                    ),
-                                    Text(
-                                      '↓${we.tempMin!.toStringAsFixed(0)}°',
-                                      style: const TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.blueAccent),
+                                    Image.network(
+                                        'http://openweathermap.org/img/wn/${we.icon}.png'),
+                                    Container(
+                                      width: 50,
+                                      child: Text(
+                                        '${we.rainyPercent}%',
+                                        style: const TextStyle(
+                                            color: Colors.blueAccent),
+                                      ),
                                     ),
                                   ],
                                 ),
-                              ),
-                            ],
-                          ),
-                        );
-                      }).toList(),
+                                Container(
+                                  width: 90,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        '↑${we.tempMax!.toStringAsFixed(0)}°',
+                                        style: const TextStyle(
+                                            fontSize: 16, color: Colors.red),
+                                      ),
+                                      Text(
+                                        '↓${we.tempMin!.toStringAsFixed(0)}°',
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.blueAccent),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        }).toList(),
+                      ),
                     ),
                   ),
-                ),
-              )
-      ],
-    )));
+                )
+        ],
+      ),
+    ])));
   }
 }
